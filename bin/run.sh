51f128b10fb3c54ea04ba8cadf9d7d5c5aa98b60
Batista Harahap <batista@bango29.com>
2022-03-18 21:33:28 +0400

    fix docker entry point

--- bin/run.sh
+++ bin/run.sh
@@ -12,9 +12,9 @@ if test "x${ENV}" = 'xdev'; then
     exit 1
   fi
 else
-  if test "x${APPNAME}" = "xaccount" ; then
+  if test "x${APPNAME}" = "xbackend" ; then
     uvicorn appbackend.web:app --host 0.0.0.0 --port 8080 --log-level info
-  elif test "x${APPNAME}" = "xtrade" ; then
+  elif test "x${APPNAME}" = "xfrontend" ; then
     uvicorn appfrontend.web:app --host 0.0.0.0 --port 8080 --log-level info
   else
     echo "invalid value for envvar APPNAME '${APPNAME}'"
