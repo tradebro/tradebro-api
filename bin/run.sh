7572ed4b8292ec9b5e76195af1243191249ab784
Batista Harahap <batista@bango29.com>
2022-04-11 19:06:49 +0400

    set default port

--- bin/run.sh
+++ bin/run.sh
@@ -2,20 +2,25 @@
 
 echo 'Environment is: '${ENV}
 
+if test "x${PORT}" = 'x'; then
+  export PORT=8080
+fi
+
+
 if test "x${ENV}" = 'xdev'; then
   if test "x${APPNAME}" = "xbackend" ; then
-    uvicorn appbackend.web:app --host 0.0.0.0 --port 8080 --log-level debug --reload --reload-dir src
+    uvicorn appbackend.web:app --host 0.0.0.0 --port $PORT --log-level debug --reload --reload-dir src
   elif test "x${APPNAME}" = "xfrontend" ; then
-    uvicorn appfrontend.web:app --host 0.0.0.0 --port 8080 --log-level debug --reload --reload-dir src
+    uvicorn appfrontend.web:app --host 0.0.0.0 --port $PORT --log-level debug --reload --reload-dir src
   else
     echo "invalid value for envvar APPNAME '${APPNAME}'"
     exit 1
   fi
 else
   if test "x${APPNAME}" = "xbackend" ; then
-    uvicorn appbackend.web:app --host 0.0.0.0 --port 8080 --log-level info
+    uvicorn appbackend.web:app --host 0.0.0.0 --port $PORT --log-level info
   elif test "x${APPNAME}" = "xfrontend" ; then
-    uvicorn appfrontend.web:app --host 0.0.0.0 --port 8080 --log-level info
+    uvicorn appfrontend.web:app --host 0.0.0.0 --port $PORT --log-level info
   else
     echo "invalid value for envvar APPNAME '${APPNAME}'"
     exit 1
